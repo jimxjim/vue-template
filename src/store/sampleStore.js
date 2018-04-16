5fe36e55817e9728a872ce832e17d9d826fc477c
jim.hsieh <jim74255@gmail.com>
2018-04-16 14:19:26 +0800

    sampleStore modify

--- src/store/sampleStore.js
+++ src/store/sampleStore.js
@@ -15,7 +15,7 @@ const actions = {
   yourAction(store) {
     return Request(store, types.YOUR_TYPE, {
       url: '/path', // 修改为该后端开出接口位置
-      methods: 'GET', // 接口使用方法
+      method: 'GET', // 接口使用方法
       headers: {}, // 依需求添加
       auth: false, // 是否需要认证，可用于判断是否要带accessToken，详细实作可参考 src/request 中的 sendRequest
     });
